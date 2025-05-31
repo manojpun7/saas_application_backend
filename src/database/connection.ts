9149452e4617a4293c081c4d72e2943a58418c06
Manoj <punmanoj2060@gmail.com>
2025-05-31 21:51:53 +0545

    database connection and table created

---
+++ src/database/connection.ts
@@ -0,0 +1,27 @@
+import { Sequelize } from "sequelize-typescript";
+
+const sequelize = new Sequelize({
+  database: process.env.DB_NAME,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  dialect: "mysql",
+  port: Number(process.env.DB_NAME),
+  models:[__dirname+'/models']
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("connected successfully");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+sequelize.sync({force:false})
+.then(()=>{
+  console.log("migrated successfully!")
+})
+
+export default sequelize;
