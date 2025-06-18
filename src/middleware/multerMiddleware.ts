4b481926bd5048f9d954ded2bccf3dd2b02139db
Manoj <punmanoj2060@gmail.com>
2025-06-18 20:37:00 +0545

    multer

---
+++ src/middleware/multerMiddleware.ts
@@ -0,0 +1,14 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  //location incomming file kata rakne vanne ho
+  destination: function (req: Request, file: Express.Multer.File, cb: any) {
+    cb(null, "./src/storage");
+  },
+  //mathi ko location deko ma rakey paxi , k name ma rakne vanne ho
+  filename: function (req: Request, file: Express.Multer.File, cb: any) {
+    cb(null, Date.now()+ "-"+file.originalname)
+  },
+});
+
+export default { multer, storage };
