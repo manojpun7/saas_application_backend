7e21381169299d5ceb00280ccd19dfeff9bdd29f
Manoj <punmanoj2060@gmail.com>
2025-06-19 19:57:58 +0545

    controller for file

---
+++ src/controller/institute/course/courseController.ts
@@ -0,0 +1,71 @@
+import { Request, Response } from "express";
+import sequelize from "../../../database/connection";
+import { IExtendedRequest } from "../../../middleware/type";
+
+
+
+const createCourse = async (req:IExtendedRequest,res:Response)=>{
+    const instituteNumber = req.user?.currentInstituteNumber
+const {coursePrice, courseName,courseDescription, courseDuration, courseLevel } = req.body 
+if(!coursePrice || !courseName || !courseDescription || !courseDuration || !courseLevel){
+    return res.status(400).json({
+        messsage : "Please provide coursePrice, courseName, courseDescription, courseDuration, courseLevel"
+    })
+}
+const courseThumbnail = null
+
+const returnedData = await sequelize.query(`INSERT INTO course_${instituteNumber}(coursePrice,courseName,courseDescription,courseDuration,courseLevel,courseThumbnail) VALUES(?,?,?,?,?,?)`,{
+    replacements : [coursePrice, courseName,courseDescription,courseDuration,courseLevel,courseThumbnail || "https://digitalpathshalanepal.com/image/hello.png"]
+})
+
+console.log(returnedData)
+res.status(200).json({
+    message : 'course created successfully'
+})
+}
+
+const deleteCourse = async(req:IExtendedRequest,res:Response)=>{
+    const instituteNumber = req.user?.currentInstituteNumber 
+    const courseId = req.params.id 
+    // first check if course exists or not , if exists --> delete else not delete 
+    const [courseData] = await sequelize.query(`SELECT * FROM course_${instituteNumber} WHERE id=?`,{
+        replacements : [courseId]
+    })
+
+    if(courseData.length == 0){
+        return res.status(404).json({
+            message : "no course with that id"
+        })
+    }
+
+    await sequelize.query(`DELETE FROM course_${instituteNumber} WHERE id = ?`,{
+        replacements : [courseId]
+    })
+    res.status(200).json({
+        message : "course deleted successfully"
+    })
+}
+
+
+const getAllCourse = async (req:IExtendedRequest,res:Response)=>{
+    const instituteNumber = req.user?.currentInstituteNumber; 
+    const courses = await sequelize.query(`SELECT * FROM course_${instituteNumber}`)
+    res.status(200).json({
+        message : "Course fetched", 
+        data : courses 
+    })
+}
+
+const getSingleCourse = async(req:IExtendedRequest,res:Response)=>{
+    const instituteNumber = req.user?.currentInstituteNumber; 
+    const courseId = req.params.id
+    const course = await sequelize.query(`SELECT * FROM course_${instituteNumber} WHERE id = ?`,{
+        replacements : [courseId]
+    })
+    res.status(200).json({
+        message : "single course fetched", 
+        data : course
+    })
+}
+
+export {createCourse,deleteCourse,getSingleCourse,getAllCourse}
--- src/middleware/multerMiddleware.ts
+++ src/middleware/multerMiddleware.ts
@@ -2,15 +2,17 @@ import multer from "multer";
 
 
 // locally storage ko lagi
-const storage = multer.diskStorage({
-  //location incomming file kata rakne vanne ho
-  destination: function (req: Request, file: Express.Multer.File, cb: any) {
-    cb(null, "./src/storage");
-  },
-  //mathi ko location deko ma rakey paxi , k name ma rakne vanne ho
-  filename: function (req: Request, file: Express.Multer.File, cb: any) {
-    cb(null, Date.now()+ "-"+file.originalname)
-  },
-});
+// const storage = multer.diskStorage({
+//   //location incomming file kata rakne vanne ho
+//   destination: function (req: Request, file: Express.Multer.File, cb: any) {
+//     cb(null, "./src/storage");
+//   },
+//   //mathi ko location deko ma rakey paxi , k name ma rakne vanne ho
+//   filename: function (req: Request, file: Express.Multer.File, cb: any) {
+//     cb(null, Date.now()+ "-"+file.originalname)
+//   },
+// });
 
-export default { multer, storage };
+// export default { multer, storage };
+
+
